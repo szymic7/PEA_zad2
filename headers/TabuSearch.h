7e08d04f9a1698e0097d2e127ea83a8523d84547
szymic <szymon.julian.michalowski@gmail.com>
2024-12-13 14:05:04 +0100

    Invert require corrections

--- headers/TabuSearch.h
+++ headers/TabuSearch.h
@@ -29,20 +29,25 @@ private:
 
     // Lista tabu
     //std::set<std::pair<int, int>> tabuList;
-    std::list<std::pair<std::vector<int>, int>> tabuList;
+    //std::list<std::pair<std::vector<int>, int>> tabuList;
+    int** tabuList;
 
     // Kadencja dla rozwiazan na liscie tabu
     int tabuCadency;
 
     vector<int> randomPermutation(int size);
-    void updateTabuList(const std::vector<int>& solution);
-    void diversify(std::vector<int> currentSolution);
+    void updateTabuList(/*const std::vector<int>& solution*/ int bestI, int bestJ);
+    void diversify(std::vector<int>& currentSolution);
     bool isTabu(const std::vector<int>& solution);
 
     // Generowanie rozwiazan w sasiedztwa rozwiazania currentSolution, w zaleznosci od definicji
     std::vector<std::vector<int>> neighborhoodInsert(const std::vector<int>& solution);
     std::vector<std::vector<int>> neighborhoodSwap(const std::vector<int>& solution);
     std::vector<std::vector<int>> neighborhoodInvert(const std::vector<int>& solution);
+
+    std::vector<int> findBestNeighborInsert(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ);
+    std::vector<int> findBestNeighborSwap(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ);
+    std::vector<int> findBestNeighborInvert(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ);
 
 
 public:
--- sources/TabuSearch.cpp
+++ sources/TabuSearch.cpp
@@ -1,4 +1,5 @@
 #include "../headers/TabuSearch.h"
+#include "../headers/Greedy.h"
 
 #include <ctime>
 #include <iostream>
@@ -11,11 +12,13 @@
 TabuSearch::TabuSearch() : Algorithm()
 {
     timeLimit = 0;
-    iterationsWithoutImprovementLimit = 110;
+    iterationsWithoutImprovementLimit = 300;
 
     neighborhoodDef = 1;
     noImprovementIterations = 0;
-    tabuCadency = 10;
+    tabuCadency = 25;
+
+    tabuList = nullptr;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -30,141 +33,63 @@ void TabuSearch::setNeighborhoodDef(int nDef) {
     neighborhoodDef = nDef;
 }
 
-//--------------------------------------------------------------------------------------------------
-
-/*void TabuSearch::run()
-{
-    vector<vector<int>> tabuMatrix;
-    vector<int>best;
-    vector<int>permutation = randomPermutation(n);
-    vector<int>next(permutation);
-    int result = 1 << 30;
-    int firstToSwap;
-    int secondToSwap;
-    int nextCost;
-    std::clock_t start;
-    double timePassed;
-    double foundTime;
-
-    tabuMatrix.resize(n);
-
-    for (int j = 0; j < n; ++j)
-    {
-        tabuMatrix[j].resize(n, 0);
-    }
-
-    start = std::clock();
-
-    while(true)
-    {
-        for (int step = 0; step <  15 * n; ++step)
-        {
-            firstToSwap = 0;
-            secondToSwap = 0;
-            nextCost = 1 << 30;
-
-            for (int first = 0; first < n; ++first)
-            {
-                for (int second = first + 1; second < n; ++second)
-                {
-                    std::swap(permutation[first], permutation[second]);
-                    int currentCost = calculatePathCost(permutation);
-
-                    if (currentCost < result)
-                    {
-                        result = currentCost;
-                        best = permutation;
-                        foundTime = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-                    }
-
-                    if (currentCost < nextCost)
-                    {
-                        if (tabuMatrix[second][first]<step)
-                        {
-                            nextCost = currentCost;
-                            next = permutation;
-
-                            firstToSwap = second;
-                            secondToSwap = first;
-                        }
-                    }
-
-                    std::swap(permutation[first], permutation[second]);
-
-                    timePassed = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-
-                    if (timePassed >= timeLimit)
-                    {
-                        cout << "Droga: ";
-                        for (int d = 0; d < n; d++)
-                        {
-                            cout << best[d] << " ";
-                        }
-
-                        cout << "\nKoszt: " << result << endl;
-                        cout << "Znaleziono po: " << foundTime << " s " << endl;
-                        return;
-                    }
-
-                }
-            }
-            permutation = next;
-            tabuMatrix[firstToSwap][secondToSwap] += n;
-        }
-
-        permutation.clear();
-
-        permutation = randomPermutation(n);
-
-        for (std::vector<vector<int> >::iterator it = tabuMatrix.begin(); it != tabuMatrix.end(); ++it)
-        {
-            it->clear();
-        }
-        tabuMatrix.clear();
-
-        tabuMatrix.resize(n);
-
-        for (std::vector<vector<int> >::iterator it = tabuMatrix.begin(); it != tabuMatrix.end(); ++it)
-        {
-            it->resize(n, 0);
-        }
-    }
-}*/
-
-//------------------------------------------------------------------------------------------------
-
-vector<int> TabuSearch::randomPermutation(int _size)
-{
-    std::vector<int> temp;
-    temp.reserve(_size);
-
-    for (int i = 0; i < _size; i++)
-    {
-        temp.push_back(i);
-    }
-
-    shuffle(temp.begin(), temp.end(), std::mt19937(std::random_device()()));
-
-    return temp;
-}
-
 //---------------------------------------------------------------------------------------------
 
 void TabuSearch::algorithm() {
 
     // Wyczyszczenie rozwiazania
     resultVector.clear();
+    double timeOfResult = 0.0;
+    noImprovementIterations = 0;
+
+    // Wyczyszczenie listy tabu
+    if (tabuList != nullptr) {
+        for (int i = 0; i < n; ++i) {
+            delete [] tabuList[i];
+        }
+        delete [] tabuList;
+    }
+
+    // Inicjalizacja nowej listy tabu
+    tabuList = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        tabuList[i] = new int[n];
+        for(int j = 0; j < n; j++) {
+            tabuList[i][j] = 0; // Poczatkowo tabu list wypelniona wartosciami 0
+        }
+    }
 
     // Rozwiazanie poczatkowe - wygenerowane metoda losowa
     std::vector<int> currentSolution;
-    currentSolution.resize(n);
+    /*currentSolution.resize(n);
     std::iota(currentSolution.begin(), currentSolution.end(), 0);
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::shuffle(currentSolution.begin(), currentSolution.end(), gen);
+    std::shuffle(currentSolution.begin(), currentSolution.end(), gen);*/
+    Greedy greedy;
+    greedy.setN(n);
+    greedy.setCostMatrix(costMatrix);
+    greedy.greedyAlgorithm();
+
+    int* resultVertices = greedy.getResultVertices();
+    currentSolution.resize(n); // Ustaw rozmiar wektora
+    for (int i = 0; i < n; ++i) {
+        currentSolution[i] = resultVertices[i];
+    }
+    delete [] resultVertices;
+
+
+    cout << "Rozwiazanie poczatkowe: ";
+    for (int i : currentSolution) {
+        cout << i << " ";
+    }
+    cout << endl;
 
     std::vector<int> bestSolution = currentSolution;
     int bestCost = calculatePathCost(bestSolution);
+
+    // Wierzcholki i, j do dodania do listy tabu
+    int bestI, bestJ;
 
     auto startTime = std::chrono::steady_clock::now();
 
@@ -172,25 +97,31 @@ void TabuSearch::algorithm() {
         int currentCost = std::numeric_limits<int>::max();
         std::vector<int> bestNeighbor;
 
-        std::vector<std::vector<int>> neighbors;
+        //std::vector<std::vector<int>> neighbors;
 
         switch (neighborhoodDef) {
             case 1: // insert
-                neighbors = neighborhoodInsert(currentSolution);
+                //neighbors = neighborhoodInsert(currentSolution);
+                bestNeighbor = findBestNeighborInsert(currentSolution, currentCost, bestI, bestJ);
                 break;
             case 2: // swap
-                neighbors = neighborhoodSwap(currentSolution);
+                //neighbors = neighborhoodSwap(currentSolution);
+                bestNeighbor = findBestNeighborSwap(currentSolution, currentCost, bestI, bestJ);
                 break;
             case 3: // invert
-                neighbors = neighborhoodInvert(currentSolution);
+                //neighbors = neighborhoodInvert(currentSolution);
+                bestNeighbor = findBestNeighborInvert(currentSolution, currentCost, bestI, bestJ);
                 break;
             default:
-                neighbors = neighborhoodInsert(currentSolution);
+                //neighbors = neighborhoodInsert(currentSolution);
+                bestNeighbor = findBestNeighborInsert(currentSolution, currentCost, bestI, bestJ);
                 cout << endl << "Ustawiono domyslna definicje sasiedztwa - Insert." << endl;
                 break;
         }
 
-        for(const auto& neighbor : neighbors) {
+        currentSolution = bestNeighbor;
+
+        /*for(const auto& neighbor : neighbors) {
             if (!isTabu(neighbor)) {
             //if (tabuList.count({neighbor.front(), neighbor.back()}) == 0) {
                 int neighborCost = calculatePathCost(neighbor);
@@ -199,26 +130,34 @@ void TabuSearch::algorithm() {
                     bestNeighbor = neighbor;
                 }
             }
-        }
+        }*/
 
         if (currentCost < bestCost) { // Nowe rozwiazanie jest dotychczas najlepszym
             bestCost = currentCost;
             bestSolution = bestNeighbor;
             noImprovementIterations = 0;
+            timeOfResult = std::chrono::duration<double>(std::chrono::steady_clock::now() - startTime).count();
         } else {
             noImprovementIterations++;
         }
 
+        // Aktualizacja listy tabu
+        updateTabuList(bestI, bestJ);
+
+        // Ewentualna dywersyfikacja
         if (noImprovementIterations >= iterationsWithoutImprovementLimit) {
             diversify(currentSolution); // Wymuś dywersyfikację
         }
 
-        currentSolution = bestNeighbor;
-        //updateTabuList({bestNeighbor.front(), bestNeighbor.back()});
-        updateTabuList(bestNeighbor);
+
+
+        //updateTabuList(bestNeighbor);
+
     }
 
     resultVector = bestSolution;
+
+    cout << "Czas znalezienia najlepszego rozwiazania: " << timeOfResult << " sekund" << endl;
 }
 
 //---------------------------------------------------------------------------------------------
@@ -270,8 +209,164 @@ std::vector<std::vector<int>> TabuSearch::neighborhoodInvert(const std::vector<i
 
 //---------------------------------------------------------------------------------------------
 
-void TabuSearch::updateTabuList(const std::vector<int>& solution) {
+std::vector<int> TabuSearch::findBestNeighborInsert(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ) {
+    std::vector<int> bestNeighbor = solution;
+    bestCost = std::numeric_limits<int>::max();
+    std::vector<int> neighbor = solution;
 
+    for (int i = 0; i < solution.size(); i++) {
+        for (int j = 0; j < solution.size(); j++) {
+            if (i != j) {
+                neighbor = solution;
+
+                int temp = neighbor[i];
+                neighbor.erase(neighbor.begin() + i);
+
+                // Skoryguj pozycję `j`, jeśli jest większa niż `i`
+                int adjustedJ = j;
+                if (j > i) {
+                    adjustedJ--; // Po usunięciu elementu, indeks `j` przesuwa się o 1 w lewo
+                }
+
+                neighbor.insert(neighbor.begin() + adjustedJ, temp);
+
+                int cost = calculatePathCost(neighbor);
+                if (/*!isTabu(neighbor)*/ tabuList[i][j] == 0 || cost < bestCost) {
+                    if (cost < bestCost) {
+                        bestCost = cost;
+                        bestNeighbor = neighbor;
+                        bestI = i;
+                        bestJ = j;
+                    }
+                }
+            }
+        }
+    }
+
+    //cout << endl << "Zakoczono przeszukiwanie sasiedztwa." << endl;
+
+    return bestNeighbor;
+}
+
+//---------------------------------------------------------------------------------------------
+
+std::vector<int> TabuSearch::findBestNeighborSwap(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ) {
+    std::vector<int> bestNeighbor = solution;
+    bestCost = std::numeric_limits<int>::max();
+    std::vector<int> neighbor = solution;
+
+    for (int i = 0; i < solution.size(); i++) {
+        for (int j = i + 1; j < solution.size(); j++) {
+            std::swap(neighbor[i], neighbor[j]);
+
+            int cost = calculatePathCost(neighbor);
+            if (/*!isTabu(neighbor)*/ tabuList[i][j] == 0 || cost < bestCost) {
+                if (cost < bestCost) {
+                    bestCost = cost;
+                    bestNeighbor = neighbor;
+                    bestI = i;
+                    bestJ = j;
+                }
+            }
+
+            // Przywroc stan
+            std::swap(neighbor[i], neighbor[j]);
+        }
+    }
+
+    //cout << endl << "Zakoczono przeszukiwanie sasiedztwa." << endl;
+
+    return bestNeighbor;
+}
+
+//---------------------------------------------------------------------------------------------
+
+std::vector<int> TabuSearch::findBestNeighborInvert(const std::vector<int>& solution, int& bestCost, int& bestI, int& bestJ) {
+    std::vector<int> bestNeighbor = solution;
+    bestCost = std::numeric_limits<int>::max();
+    std::vector<int> neighbor = solution;
+
+    for (int i = 0; i < solution.size(); i++) {
+        for (int j = i + 1; j < solution.size(); j++) {
+            std::reverse(neighbor.begin() + i, neighbor.begin() + j + 1);
+
+            int cost = calculatePathCost(neighbor);
+            if (/*!isTabu(neighbor)*/ tabuList[i][j] == 0 || cost < bestCost) {
+                if (cost < bestCost) {
+                    bestCost = cost;
+                    bestNeighbor = neighbor;
+                    bestI = i;
+                    bestJ = j;
+                }
+            }
+
+            // Przywróć stan
+            std::reverse(neighbor.begin() + i, neighbor.begin() + j + 1);
+        }
+    }
+
+    //cout << endl << "Zakoczono przeszukiwanie sasiedztwa." << endl;
+
+    return bestNeighbor;
+}
+
+/*std::vector<int> TabuSearch::findBestNeighborInvert(const std::vector<int>& solution, int& bestCost) {
+    int size = solution.size();
+
+    // Konwersja wektora na dynamiczną tablicę
+    int* currentNeighbor = new int[size];
+    int* bestNeighbor = new int[size];
+    std::copy(solution.begin(), solution.end(), currentNeighbor);
+    std::copy(solution.begin(), solution.end(), bestNeighbor);
+
+    bestCost = std::numeric_limits<int>::max();
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            // Odwrócenie fragmentu w dynamicznej tablicy
+            int start = i, end = j;
+            while (start < end) {
+                std::swap(currentNeighbor[start], currentNeighbor[end]);
+                start++;
+                end--;
+            }
+
+            // Obliczenie kosztu dla bieżącego sąsiada
+            int cost = calculatePathCost(currentNeighbor);
+            if (!isTabu(currentNeighbor) || cost <= bestCost) {
+                if (cost < bestCost) {
+                    bestCost = cost;
+                    std::copy(currentNeighbor, currentNeighbor + size, bestNeighbor);
+                }
+            }
+
+            // Przywrócenie stanu (odwrócenie ponowne)
+            start = i;
+            end = j;
+            while (start < end) {
+                std::swap(currentNeighbor[start], currentNeighbor[end]);
+                start++;
+                end--;
+            }
+        }
+    }
+
+    // Konwersja najlepszej tablicy z powrotem na wektor
+    std::vector<int> bestNeighborVector(bestNeighbor, bestNeighbor + size);
+
+    // Zwolnienie pamięci dynamicznej
+    delete[] currentNeighbor;
+    delete[] bestNeighbor;
+
+    return bestNeighborVector;
+}*/
+
+
+//---------------------------------------------------------------------------------------------
+
+void TabuSearch::updateTabuList(/*const std::vector<int>& solution*/ int bestI, int bestJ) {
+
+    /*
     for (auto it = tabuList.begin(); it != tabuList.end(); ) {
         it -> second--; // Decrease remaining iterations
         if (it -> second <= 0) {
@@ -283,6 +378,21 @@ void TabuSearch::updateTabuList(const std::vector<int>& solution) {
 
     // Add the new solution to the tabu list with the tabuCadency
     tabuList.emplace_back(solution, tabuCadency);
+     */
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (tabuList[i][j] > 0) tabuList[i][j]--;
+        }
+    }
+
+    // Dodanie ostatniego ruchu do listy tabu
+    tabuList[bestI][bestJ] = tabuCadency;
+
+    // Dla operacji Swap i Invert ruch (i, j) jest rownoznaczny ruchowi (j, i)
+    if(neighborhoodDef == 2 || neighborhoodDef == 3) {
+        tabuList[bestJ][bestI] = tabuCadency;
+    }
 }
 
 //---------------------------------------------------------------------------------------------
@@ -296,7 +406,8 @@ bool TabuSearch::isTabu(const std::vector<int>& solution) {
     }
     return false; // The solution is not tabu*/
 
-    for (const auto& tabuEntry : tabuList) {
+
+    /*for (const auto& tabuEntry : tabuList) {
         const auto& tabuSolution = tabuEntry.first;
 
         if (solution.size() != tabuSolution.size()) {
@@ -323,16 +434,33 @@ bool TabuSearch::isTabu(const std::vector<int>& solution) {
         }
     }
 
-    return false; // No cyclic match found
+    return false; // No cyclic match found*/
 
 }
 
 //---------------------------------------------------------------------------------------------
 
-void TabuSearch::diversify(std::vector<int> currentSolution) {
-    std::random_device rd;
+void TabuSearch::diversify(std::vector<int>& currentSolution) {
+
+    /*std::random_device rd;
     std::mt19937 gen(rd());
     std::shuffle(currentSolution.begin(), currentSolution.end(), gen);
+    noImprovementIterations = 0;*/
+
+    // Wprowadzenie subtelnych zmian w trasie
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    int numPerturbations = n / 10; // Zmieniamy 10% trasy
+    for (int k = 0; k < numPerturbations; ++k) {
+
+        int i = gen() % (n - 1) + 1;
+        int j;
+        do {
+            j = gen() % (n - 1) + 1;
+        } while (i == j);
+        std::swap(currentSolution[i], currentSolution[j]); // Zamiana dwóch losowych miast
+
+    }
     noImprovementIterations = 0;
 }
 
